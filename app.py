4ce2fe9a2f81e0cb6c36d4a9cf59a3a53e3a3b5f
screenai312 <airesumebasedscreeningsystem@gmail.com>
2026-04-09 14:25:04 +0530

    database fix

--- app.py
+++ app.py
@@ -100,10 +100,23 @@ def calculate_match_score(resume_text, job_description):
         score = 95
 
     return round(score, 2)
+
 # ======================
 # DATABASE CONFIG
 # ======================
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+import os
+
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url:
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
